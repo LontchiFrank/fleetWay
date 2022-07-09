7c0b595a56995d9a5c8682ea54195dec77897819
LontchiFrank <fodjof6@gmail.com>
2022-07-09 20:28:49 +0100

    done with the lyft request

--- src/components/ComingRequest/index.jsx
+++ src/components/ComingRequest/index.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import rock from "../../styles/assets/1.png";
-import { Check2Circle, GeoAlt, Trash, Trash2Fill } from "react-bootstrap-icons";
+import { Pen, Trash } from "react-bootstrap-icons";
 import firebase from "firebase";
 
-function IncomingRequest({ name, tel, location, Desti, time, seats, price }) {
+function IncomingRequest({ user }) {
+  const [driver, setDriver] = useState();
+  const ref = firebase.firestore();
+
+  var docRef = ref
+    .collection("Drivers")
+    .doc("Driver 1")
+    .collection("Order2")
+    .doc("Order2");
+
+  useEffect(() => {
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
+          setDriver(doc.data());
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }, []);
+
   return (
     <div
       className="card-request d-flex py-3  "
@@ -19,69 +42,89 @@ function IncomingRequest({ name, tel, location, Desti, time, seats, price }) {
         <img src={rock} alt="" style={{ height: "100%" }} />
       </div>
       <div className="name d-flex flex-column  " style={{ paddingLeft: "3em" }}>
-        <p className="m-0">Name:</p>
-        <p style={{ color: "#989898", fontSize: "13px" }}>{name} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Name:
+        </p>
+        <p style={{ color: "#000", fontSize: "16px" }}>{user && user.Name} </p>
       </div>
       <div
         className="name d-flex flex-column justify-content-start "
         style={{ paddingLeft: "3em" }}
       >
-        <p className="m-0">Tel:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{tel} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Tel:
+        </p>
+        <p style={{ fontSize: "16px", color: "#000000" }}>
+          {user && user.tel}{" "}
+        </p>
       </div>
       <div
         className="name d-flex flex-column justify-content-start "
         style={{ paddingLeft: "3em" }}
       >
-        <p className="m-0">Destination:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{Desti} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Seats:
+        </p>
+        <p style={{ fontSize: "16px", color: "#000000" }}>{driver.seats} </p>
       </div>
       <div
         className="name d-flex flex-column justify-content-start "
         style={{ paddingLeft: "3em" }}
       >
-        <p className="m-0">Location:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{location} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Price:
+        </p>
+        <p style={{ fontSize: "16px", color: "#000000" }}>{driver.prices} </p>
       </div>
       <div
         className="name d-flex flex-column justify-content-start "
         style={{ paddingLeft: "3em" }}
       >
-        <p className="m-0">Seats:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{seats} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Date:
+        </p>
+        <p style={{ fontSize: "16px", color: "#000000" }}>12/07/2022 </p>
       </div>
       <div
         className="name d-flex flex-column justify-content-start "
         style={{ paddingLeft: "3em" }}
       >
-        <p className="m-0">Price:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{price} </p>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Time:
+        </p>
+        <p style={{ fontSize: "16px", color: "#000000" }}>10mins </p>
       </div>
-      <div
-        className="name d-flex flex-column justify-content-start "
-        style={{ paddingLeft: "3em" }}
-      >
-        <p className="m-0">Time:</p>
-        <p style={{ fontSize: "13px", color: "#989898" }}>{time} </p>
+      <div className="d-flex flex-column " style={{ paddingLeft: "3em" }}>
+        <p className="m-0" style={{ color: "#989898", fontSize: "16px" }}>
+          Status:
+        </p>
+        {driver.accept == true ? (
+          <p style={{ fontSize: "16px", color: "green" }}>Approved</p>
+        ) : null}
+        {driver.accept == false ? (
+          <p style={{ fontSize: "16px", color: "orange" }}>Pending</p>
+        ) : null}
+        {driver.decline == true ? (
+          <p style={{ fontSize: "16px", color: "red" }}>Declined</p>
+        ) : null}
       </div>
       <div className="footer-request mt-2 d-flex ">
-        <div style={{ paddingLeft: "1em" }}>
+        <div style={{ paddingLeft: "2em" }}>
           <Button
-            variant="success"
-            className=""
+            variant="info"
+            className="px-4"
             // onClick={() => acceptClick()}
           >
-            Accept <Check2Circle style={{ fontSize: "14px" }} />{" "}
+            Edit <Pen style={{ fontSize: "22px" }} />{" "}
           </Button>
         </div>
-
-        <div style={{ paddingLeft: "1em" }}>
+        <div style={{ paddingLeft: "2em" }}>
           <Button
             variant="danger"
-            className=""
+            className="px-4"
             // onClick={() => declineClick()}
           >
-            Decline <Trash style={{ fontSize: "14px" }} />{" "}
+            Decline <Trash style={{ fontSize: "22px" }} />{" "}
           </Button>
         </div>
       </div>
